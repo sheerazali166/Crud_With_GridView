e2b8eabd9c72251cadffdd027fdc89d61eb9f4c4
Sheeraz Ali <sheerazali656@gmail.com>
2020-06-30 01:21:15 -0700

    Initial Commit

---
+++ Crud_Application_With_GridView/Crud_Application_With_GridView/Form1.Designer.cs
@@ -0,0 +1,355 @@
+﻿namespace Crud_Application_With_GridView
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBoxId = new System.Windows.Forms.TextBox();
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.textBoxSalary = new System.Windows.Forms.TextBox();
+            this.comboBoxGender = new System.Windows.Forms.ComboBox();
+            this.numericUpDownAge = new System.Windows.Forms.NumericUpDown();
+            this.comboBoxDesignation = new System.Windows.Forms.ComboBox();
+            this.buttonInsert = new System.Windows.Forms.Button();
+            this.buttonDisplayData = new System.Windows.Forms.Button();
+            this.buttonUpdateData = new System.Windows.Forms.Button();
+            this.buttonDeleteData = new System.Windows.Forms.Button();
+            this.buttonReset = new System.Windows.Forms.Button();
+            this.dataGridViewShowData = new System.Windows.Forms.DataGridView();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textBoxSearch = new System.Windows.Forms.TextBox();
+            this.buttonSearch = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownAge)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewShowData)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.OrangeRed;
+            this.label1.Location = new System.Drawing.Point(239, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(172, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Employee Details";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(53, 60);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(25, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Id:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(53, 97);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 16);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Name:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(53, 133);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(63, 16);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Gender:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(53, 171);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(40, 16);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "Age:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(53, 208);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(95, 16);
+            this.label6.TabIndex = 1;
+            this.label6.Text = "Designation:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(53, 249);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(57, 16);
+            this.label7.TabIndex = 1;
+            this.label7.Text = "Salary:";
+            // 
+            // textBoxId
+            // 
+            this.textBoxId.Location = new System.Drawing.Point(165, 56);
+            this.textBoxId.Name = "textBoxId";
+            this.textBoxId.Size = new System.Drawing.Size(219, 20);
+            this.textBoxId.TabIndex = 0;
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Location = new System.Drawing.Point(165, 93);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(219, 20);
+            this.textBoxName.TabIndex = 1;
+            // 
+            // textBoxSalary
+            // 
+            this.textBoxSalary.Location = new System.Drawing.Point(165, 245);
+            this.textBoxSalary.Name = "textBoxSalary";
+            this.textBoxSalary.Size = new System.Drawing.Size(219, 20);
+            this.textBoxSalary.TabIndex = 4;
+            // 
+            // comboBoxGender
+            // 
+            this.comboBoxGender.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
+            this.comboBoxGender.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems;
+            this.comboBoxGender.FormattingEnabled = true;
+            this.comboBoxGender.Items.AddRange(new object[] {
+            "Male",
+            "Female"});
+            this.comboBoxGender.Location = new System.Drawing.Point(165, 127);
+            this.comboBoxGender.Name = "comboBoxGender";
+            this.comboBoxGender.Size = new System.Drawing.Size(219, 21);
+            this.comboBoxGender.TabIndex = 2;
+            // 
+            // numericUpDownAge
+            // 
+            this.numericUpDownAge.Location = new System.Drawing.Point(165, 166);
+            this.numericUpDownAge.Maximum = new decimal(new int[] {
+            60,
+            0,
+            0,
+            0});
+            this.numericUpDownAge.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDownAge.Name = "numericUpDownAge";
+            this.numericUpDownAge.Size = new System.Drawing.Size(219, 20);
+            this.numericUpDownAge.TabIndex = 3;
+            this.numericUpDownAge.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // comboBoxDesignation
+            // 
+            this.comboBoxDesignation.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
+            this.comboBoxDesignation.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems;
+            this.comboBoxDesignation.FormattingEnabled = true;
+            this.comboBoxDesignation.Items.AddRange(new object[] {
+            "Manager",
+            "Accountant",
+            "Assistant Manager",
+            "Graphic Designer",
+            "Mobile Developer",
+            "Desktop Developer",
+            "Web Developer"});
+            this.comboBoxDesignation.Location = new System.Drawing.Point(165, 203);
+            this.comboBoxDesignation.Name = "comboBoxDesignation";
+            this.comboBoxDesignation.Size = new System.Drawing.Size(219, 21);
+            this.comboBoxDesignation.TabIndex = 3;
+            // 
+            // buttonInsert
+            // 
+            this.buttonInsert.Location = new System.Drawing.Point(440, 50);
+            this.buttonInsert.Name = "buttonInsert";
+            this.buttonInsert.Size = new System.Drawing.Size(111, 30);
+            this.buttonInsert.TabIndex = 6;
+            this.buttonInsert.Text = "Insert Data";
+            this.buttonInsert.UseVisualStyleBackColor = true;
+            this.buttonInsert.Click += new System.EventHandler(this.buttonInsert_Click);
+            // 
+            // buttonDisplayData
+            // 
+            this.buttonDisplayData.Location = new System.Drawing.Point(440, 97);
+            this.buttonDisplayData.Name = "buttonDisplayData";
+            this.buttonDisplayData.Size = new System.Drawing.Size(111, 30);
+            this.buttonDisplayData.TabIndex = 7;
+            this.buttonDisplayData.Text = "Display Data";
+            this.buttonDisplayData.UseVisualStyleBackColor = true;
+            this.buttonDisplayData.Click += new System.EventHandler(this.buttonDisplayData_Click);
+            // 
+            // buttonUpdateData
+            // 
+            this.buttonUpdateData.Location = new System.Drawing.Point(440, 144);
+            this.buttonUpdateData.Name = "buttonUpdateData";
+            this.buttonUpdateData.Size = new System.Drawing.Size(111, 30);
+            this.buttonUpdateData.TabIndex = 8;
+            this.buttonUpdateData.Text = "Update Data";
+            this.buttonUpdateData.UseVisualStyleBackColor = true;
+            this.buttonUpdateData.Click += new System.EventHandler(this.buttonUpdateData_Click);
+            // 
+            // buttonDeleteData
+            // 
+            this.buttonDeleteData.Location = new System.Drawing.Point(440, 194);
+            this.buttonDeleteData.Name = "buttonDeleteData";
+            this.buttonDeleteData.Size = new System.Drawing.Size(111, 30);
+            this.buttonDeleteData.TabIndex = 9;
+            this.buttonDeleteData.Text = "Delete Data";
+            this.buttonDeleteData.UseVisualStyleBackColor = true;
+            this.buttonDeleteData.Click += new System.EventHandler(this.buttonDeleteData_Click);
+            // 
+            // buttonReset
+            // 
+            this.buttonReset.Location = new System.Drawing.Point(440, 242);
+            this.buttonReset.Name = "buttonReset";
+            this.buttonReset.Size = new System.Drawing.Size(111, 30);
+            this.buttonReset.TabIndex = 10;
+            this.buttonReset.Text = "Reset";
+            this.buttonReset.UseVisualStyleBackColor = true;
+            this.buttonReset.Click += new System.EventHandler(this.buttonReset_Click);
+            // 
+            // dataGridViewShowData
+            // 
+            this.dataGridViewShowData.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewShowData.Location = new System.Drawing.Point(20, 335);
+            this.dataGridViewShowData.Name = "dataGridViewShowData";
+            this.dataGridViewShowData.Size = new System.Drawing.Size(627, 200);
+            this.dataGridViewShowData.TabIndex = 5;
+            this.dataGridViewShowData.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.dataGridViewShowData_MouseDoubleClick);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.buttonSearch);
+            this.groupBox1.Controls.Add(this.textBoxSearch);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(165, 285);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(386, 44);
+            this.groupBox1.TabIndex = 11;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Search";
+            // 
+            // textBoxSearch
+            // 
+            this.textBoxSearch.Location = new System.Drawing.Point(78, 16);
+            this.textBoxSearch.Name = "textBoxSearch";
+            this.textBoxSearch.Size = new System.Drawing.Size(243, 22);
+            this.textBoxSearch.TabIndex = 0;
+            this.textBoxSearch.TextChanged += new System.EventHandler(this.textBoxSearch_TextChanged);
+            // 
+            // buttonSearch
+            // 
+            this.buttonSearch.Image = ((System.Drawing.Image)(resources.GetObject("buttonSearch.Image")));
+            this.buttonSearch.Location = new System.Drawing.Point(327, 13);
+            this.buttonSearch.Name = "buttonSearch";
+            this.buttonSearch.Size = new System.Drawing.Size(30, 28);
+            this.buttonSearch.TabIndex = 1;
+            this.buttonSearch.UseVisualStyleBackColor = true;
+            this.buttonSearch.Click += new System.EventHandler(this.buttonSearch_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(669, 547);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.dataGridViewShowData);
+            this.Controls.Add(this.buttonReset);
+            this.Controls.Add(this.buttonDeleteData);
+            this.Controls.Add(this.buttonUpdateData);
+            this.Controls.Add(this.buttonDisplayData);
+            this.Controls.Add(this.buttonInsert);
+            this.Controls.Add(this.numericUpDownAge);
+            this.Controls.Add(this.comboBoxDesignation);
+            this.Controls.Add(this.comboBoxGender);
+            this.Controls.Add(this.textBoxSalary);
+            this.Controls.Add(this.textBoxName);
+            this.Controls.Add(this.textBoxId);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownAge)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewShowData)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBoxId;
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.TextBox textBoxSalary;
+        private System.Windows.Forms.ComboBox comboBoxGender;
+        private System.Windows.Forms.NumericUpDown numericUpDownAge;
+        private System.Windows.Forms.ComboBox comboBoxDesignation;
+        private System.Windows.Forms.Button buttonInsert;
+        private System.Windows.Forms.Button buttonDisplayData;
+        private System.Windows.Forms.Button buttonUpdateData;
+        private System.Windows.Forms.Button buttonDeleteData;
+        private System.Windows.Forms.Button buttonReset;
+        private System.Windows.Forms.DataGridView dataGridViewShowData;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button buttonSearch;
+        private System.Windows.Forms.TextBox textBoxSearch;
+    }
+}
+
---
+++ Crud_Application_With_GridView/Crud_Application_With_GridView/Form1.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Crud_Application_With_GridView
+{
+    public partial class Form1 : Form
+    {
+        string cs = ConfigurationManager.ConnectionStrings["csdb"].ConnectionString;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonInsert_Click(object sender, EventArgs e)
+        {
+            SqlConnection conn = new SqlConnection(cs);
+            string querySelect = "select * from Employee where Id = @id";
+            SqlCommand cmdSelect = new SqlCommand(querySelect, conn);
+            cmdSelect.Parameters.AddWithValue("@id", textBoxId.Text);
+
+            conn.Open();
+            
+            SqlDataReader reader = cmdSelect.ExecuteReader();
+
+            if (reader.HasRows == true)
+            {
+
+                MessageBox.Show($"{textBoxId.Text} Id Already Exists.", "Failure", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                conn.Close();
+            }
+            else
+            {
+
+                conn.Close();
+                string query = "insert into Employee values(@id, @name, @gender, @age, @designation, @salary)";
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@id", textBoxId.Text);
+                cmd.Parameters.AddWithValue("@name", textBoxName.Text);
+                cmd.Parameters.AddWithValue("@gender", comboBoxGender.SelectedItem);
+                cmd.Parameters.AddWithValue("@age", numericUpDownAge.Value);
+                cmd.Parameters.AddWithValue("@designation", comboBoxDesignation.SelectedItem);
+                cmd.Parameters.AddWithValue("@salary", textBoxSalary.Text);
+
+                conn.Open();
+
+                int n = cmd.ExecuteNonQuery();
+
+                if (n > 0)
+                {
+
+                    MessageBox.Show("Insertion Successful!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    BindWithGridView();
+                    ResetControlls();
+                }
+                else
+                {
+
+                    MessageBox.Show("Insertion Failed.", "Failure", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+
+                conn.Close();
+            }
+
+        }
+
+        public void BindWithGridView() {
+
+            SqlConnection conn = new SqlConnection(cs);
+            string query = "select * from Employee";
+            SqlDataAdapter adapter = new SqlDataAdapter(query, conn);
+            DataTable data = new DataTable();
+            adapter.Fill(data);
+            dataGridViewShowData.DataSource = data;
+
+
+        }
+
+        private void buttonDisplayData_Click(object sender, EventArgs e)
+        {
+            BindWithGridView();
+        }
+
+        private void dataGridViewShowData_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            textBoxId.Text = dataGridViewShowData.SelectedRows[0].Cells[0].Value.ToString();
+            textBoxName.Text = dataGridViewShowData.SelectedRows[0].Cells[1].Value.ToString();
+            comboBoxGender.Text = dataGridViewShowData.SelectedRows[0].Cells[2].Value.ToString();
+            numericUpDownAge.Value = Convert.ToInt32(dataGridViewShowData.SelectedRows[0].Cells[3].Value.ToString());
+            comboBoxDesignation.Text = dataGridViewShowData.SelectedRows[0].Cells[4].Value.ToString();
+            textBoxSalary.Text = dataGridViewShowData.SelectedRows[0].Cells[5].Value.ToString();
+        }
+
+        private void buttonUpdateData_Click(object sender, EventArgs e)
+        {
+            SqlConnection conn = new SqlConnection(cs);
+            string query = "update Employee set Name = @name, Gender = @gender, Age = @age, Designation = @designation, Salary = @salary where Id = @id";
+            SqlCommand cmd = new SqlCommand(query, conn);
+            cmd.Parameters.AddWithValue("@id", textBoxId.Text);
+            cmd.Parameters.AddWithValue("@name", textBoxName.Text);
+            cmd.Parameters.AddWithValue("@gender", comboBoxGender.SelectedItem);
+            cmd.Parameters.AddWithValue("@age", numericUpDownAge.Value);
+            cmd.Parameters.AddWithValue("@designation", comboBoxDesignation.SelectedItem);
+            cmd.Parameters.AddWithValue("@salary", textBoxSalary.Text);
+
+            conn.Open();
+
+            int n = cmd.ExecuteNonQuery();
+
+            if (n > 0)
+            {
+
+                MessageBox.Show("Updation Successful!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                BindWithGridView();
+                ResetControlls();
+            }
+            else
+            {
+
+                MessageBox.Show("Updation Failed.", "Failure", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            conn.Close();
+        }
+
+        private void buttonDeleteData_Click(object sender, EventArgs e)
+        {
+            SqlConnection conn = new SqlConnection(cs);
+            string query = "delete from Employee where Id = @id";
+            SqlCommand cmd = new SqlCommand(query, conn);
+            cmd.Parameters.AddWithValue("@id", textBoxId.Text);
+            conn.Open();
+
+            int n = cmd.ExecuteNonQuery();
+
+            if (n > 0)
+            {
+
+                MessageBox.Show("Deletion Successful!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                BindWithGridView();
+                ResetControlls();
+            }
+            else
+            {
+
+                MessageBox.Show("Deletion Failed.", "Failure", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            conn.Close();
+        }
+
+        private void buttonReset_Click(object sender, EventArgs e)
+        {
+            ResetControlls();
+        }
+
+        public void ResetControlls() {
+
+            textBoxId.Clear();
+            textBoxName.Clear();
+            comboBoxGender.SelectedItem = null;
+            numericUpDownAge.Value = 1;
+            comboBoxDesignation.SelectedItem = null;
+            textBoxSalary.Clear();
+            textBoxId.Focus();
+        }
+
+        private void buttonSearch_Click(object sender, EventArgs e)
+        {
+            SqlConnection conn = new SqlConnection(cs);
+            string query = "select * from Employee where Name like @name + '%'";
+            SqlDataAdapter adapter = new SqlDataAdapter(query, conn);
+            adapter.SelectCommand.Parameters.AddWithValue("@name", textBoxSearch.Text.Trim());
+            DataTable data = new DataTable();
+            adapter.Fill(data);
+
+            if (data.Rows.Count > 0)
+            {
+
+                dataGridViewShowData.DataSource = data;
+            }
+            else {
+
+                MessageBox.Show("Nothing Found", "Search Result", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                dataGridViewShowData.DataSource = null;
+            }
+
+        }
+
+        private void textBoxSearch_TextChanged(object sender, EventArgs e)
+        {
+            SqlConnection conn = new SqlConnection(cs);
+            string query = "select * from Employee where Name like @name + '%'";
+            SqlDataAdapter adapter = new SqlDataAdapter(query, conn);
+            adapter.SelectCommand.Parameters.AddWithValue("@name", textBoxSearch.Text.Trim());
+            DataTable data = new DataTable();
+            adapter.Fill(data);
+
+            if (data.Rows.Count > 0)
+            {
+
+                dataGridViewShowData.DataSource = data;
+            }
+            else
+            {
+
+                MessageBox.Show("Nothing Found", "Search Result", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                dataGridViewShowData.DataSource = null;
+            }
+        }
+    }
+}
